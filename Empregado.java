c2aaec5ed2e8ffbcde0110dead7f69cebdfaff6e
Raquel Spinola Eger <raqueleger.8755@aluno.saojudas.br>
2022-08-12 21:33:49 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,39 @@
+public class Empregado{
+
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    public double calculaSalario(){
+        if (tipo == 1 )
+        returnsalario;
+    }
+    else if (tipo == 2){
+        //comissão caria de 0 a 1
+        return salario + salario * comissao;
+    }
+    else if (tipo == 3){
+        returnsalario + bonus;
+    }
+    else{
+        return 0.;
+    }
+
+    public String getNome (){
+        return this.nome;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade (){
+        return this.idade;
+    }
+
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+}
